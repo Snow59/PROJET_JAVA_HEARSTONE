0de48f66b202cc44c74226baa60b0f3158664e0f
Snow59 <82878319+Snow59@users.noreply.github.com>
2024-01-25 12:08:53 +0100

    Update Carte.java

--- Carte.java
+++ Carte.java
@@ -1,11 +1,12 @@
 package IMT_HearStone;
 
-
-// En utilisant private et final pour les attributs de la classe Carte, on s'assure que les caractéristiques des cartes, comme leur nom et coût, ne sont pas modifiées accidentellement après leur créatio.
-
+import java.util.Random;
+import java.util.List;
 
 public class Carte {
-    private static int compteurId = 0; 
+
+	
+    private static int compteurId = 0;
 
     private final int id;
     private final String nom;
@@ -13,17 +14,87 @@ public class Carte {
     private final int degatsAttaque;
     private final int addArmure;
     private final int restoreHealth;
-    
-    public Carte(String nom, int coutMana, int degatsAttaque,int addArmure, int restoreHealth) {
+    private final String type; // "Classique", "Protecteur", "Soigneur", "Mascotte"
+
+    public Carte(String nom, int coutMana, int degatsAttaque, int addArmure, int restoreHealth, String type) {
         this.id = compteurId++;
         this.nom = nom;
         this.coutMana = coutMana;
         this.degatsAttaque = degatsAttaque;
         this.addArmure = addArmure;
         this.restoreHealth = restoreHealth;
+        this.type = type;
     }
 
-    
+    public void action(Champion cible, PlateauDeJeu plateau, List<Carte> cartesEnJeu, Carte prochaineCarte) {
+        Random random = new Random();
+
+        switch (type) {
+            case "Classique":
+                // Les monstres classiques attaquent directement l'ennemi pas les cartes 
+                cible.subirDegats(degatsAttaque);
+                break;
+            case "Protecteur":
+                // Les protecteurs offrent une protection font office de tank ( comme Cho'Gath ) ils vont tank les degats dans leur armures 
+		// Ils seront biensur élevé en mana car très utile.
+                plateau.ajouterProtecteur(this);
+                break;
+            case "Soigneur":
+                // Les soigneurs peuvent soigner un allié ou un ennemi.
+                if (!cartesEnJeu.isEmpty()) {
+                    Carte carteCiblee = cartesEnJeu.get(random.nextInt(cartesEnJeu.size()));
+                    carteCiblee.soigner(restoreHealth);
+                } else {
+                    cible.soigner(restoreHealth);
+                }
+		
+                break;
+            case "Mascotte":
+                int effet = random.nextInt(3); // 0 pour Attaque, 1 pour Armure, 2 pour Soins
+                int cibleEffet = random.nextInt(3); // 0 pour Champion, 1 pour Carte Posée, 2 pour Prochaine Carte
+		/**
+		*La mascotte a été complexe et long à faire mais le principe c'est  quand on invoque cette carte 
+  		* qui ne coutera que 1 de mana , elle fait qql chose de totalement aléatoire !
+    		* elle choisit entre notre champion , notre prochaine carte 
+		*/
+                Carte carteCiblee;
+                if (cibleEffet == 0) { // Effet sur le Champion
+                    appliquerEffetMascotte(champion, effet);
+                } else if (cibleEffet == 1 && !cartesEnJeu.isEmpty()) { // Effet sur une Carte Posée
+                    carteCiblee = cartesEnJeu.get(random.nextInt(cartesEnJeu.size()));
+                    appliquerEffetMascotte(carteCiblee, effet);
+                } else if (cibleEffet == 2 && prochaineCarte != null) { // Effet sur la Prochaine Carte
+                    appliquerEffetMascotte(prochaineCarte, effet);
+                }
+                break;
+        }
+    }
+
+   private void appliquerEffetMascotte(Carte carte, int effet) {
+        switch (effet) {
+            case 0: // Boost Attaque
+                if (carte.peutRecevoirAttaque()) {
+                    carte.boostAttaque(1);
+                }
+                break;
+            case 1: // Boost Armure
+                // Supposons que toutes les cartes peuvent recevoir de l'armure
+                carte.boostArmure(1);
+                break;
+            case 2: // Boost Soins
+                // Supposons que toutes les cartes peuvent être soignées
+                carte.soigner(1);
+                break;
+        }
+    }
+
+	
+    public boolean peutRecevoirAttaque() {
+	// je verifie car si j'ai posé une carte de protection qui elle n'a pas pour but d'attaquer je ne vais pas lui donner 1 d'attaque
+        return this.degatsAttaque > 0 || this.type.equals("Classique");
+    }
+	
+
     public int getId() {
         return id;
     }
@@ -36,19 +107,20 @@ public class Carte {
         return coutMana;
     }
 
-    
-    public int getaddArmure() {
-		return addArmure;
-	}
+    public int getAddArmure() {
+        return addArmure;
+    }
 
+    public int getRestoreHealth() {
+        return restoreHealth;
+    }
 
-	public int getrestoreHealth() {
-		return restoreHealth;
-	}
-
-
-	public int getDegatsAttaque() {
+    public int getDegatsAttaque() {
         return degatsAttaque;
+    }
+
+    public String getType() {
+        return type;
     }
 
     @Override
@@ -60,7 +132,7 @@ public class Carte {
                ", degatsAttaque=" + degatsAttaque +
                ", armureAjoutee=" + addArmure +
                ", santeRestauree=" + restoreHealth +
+               ", type='" + type + '\'' +
                '}';
     }
 }
-
