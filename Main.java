b6550fc6edc3854ac273cc4060337cff3cb1e1ed
Snow59 <82878319+Snow59@users.noreply.github.com>
2024-01-26 16:59:17 +0100

    V1 du Main

--- Main.java
+++ Main.java
@@ -1,48 +1,31 @@
 public class Main {
     public static void main(String[] args) {
-        // Création des champions
+        // Création des Mages
         Mage jaina = Mage.JAINA;
         Mage medivh = Mage.MEDIVH;
 
-        // Attribuer des decks aléatoires
         jaina.setDeck(Deck.creerDeck());
         medivh.setDeck(Deck.creerDeck());
 
-        // Mélanger les decks
-        jaina.getDeck().melangerDeck();
-        medivh.getDeck().melangerDeck();
-
-        // Création du plateau de jeu
         PlateauDeJeu plateau = new PlateauDeJeu(jaina, medivh);
 
-        // Simulation de la partie (simplifiée)
         while (jaina.getPointsDeVie() > 0 && medivh.getPointsDeVie() > 0) {
             // Tour de Jaina
-            jouerTour(jaina, plateau);
+            jaina.jouerTour();
+            jaina.special(medivh); 
 
-            // Vérifier si Medivh est KO
             if (medivh.getPointsDeVie() <= 0) break;
 
-            // Tour de Medivh
-            jouerTour(medivh, plateau);
+            medivh.jouerTour();
+            medivh.special(jaina); 
 
-            // Vérifier si Jaina est KO
             if (jaina.getPointsDeVie() <= 0) break;
         }
 
-        // Affichage du résultat
         if (jaina.getPointsDeVie() > 0) {
             System.out.println("Jaina gagne !");
         } else {
             System.out.println("Medivh gagne !");
-        }
-    }
-
-    private static void jouerTour(Mage mage, PlateauDeJeu plateau) {
-        mage.recupererMana();
-        Carte cartePiochee = mage.getDeck().piocherCarte();
-        if (cartePiochee != null) {
-            plateau.jouerCarte(mage, cartePiochee);
         }
     }
 }
