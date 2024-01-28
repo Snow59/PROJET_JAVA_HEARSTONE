5defd2dbf00ed14c9592dbc2637921f9827db248
Snow59 <82878319+Snow59@users.noreply.github.com>
2024-01-28 20:00:13 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,31 +1,29 @@
+package IMT_HearStone;
+
+import HearStoneClasseChampion.*;
+
+
 public class Main {
     public static void main(String[] args) {
         // Création des Mages
+    	
         Mage jaina = Mage.JAINA;
         Mage medivh = Mage.MEDIVH;
-
-        jaina.setDeck(Deck.creerDeck());
-        medivh.setDeck(Deck.creerDeck());
-
+       
+        
+        Deck.associerDeck(jaina);
+        Deck.associerDeck(medivh);
+        
+        jaina.getDeck();
+        medivh.getDeck();
+               
+        
         PlateauDeJeu plateau = new PlateauDeJeu(jaina, medivh);
-
-        while (jaina.getPointsDeVie() > 0 && medivh.getPointsDeVie() > 0) {
-            // Tour de Jaina
-            jaina.jouerTour();
-            jaina.special(medivh); 
-
-            if (medivh.getPointsDeVie() <= 0) break;
-
-            medivh.jouerTour();
-            medivh.special(jaina); 
-
-            if (jaina.getPointsDeVie() <= 0) break;
-        }
-
-        if (jaina.getPointsDeVie() > 0) {
-            System.out.println("Jaina gagne !");
-        } else {
-            System.out.println("Medivh gagne !");
-        }
+        
+        Monitor MaitreDeLaPartie = new Monitor(jaina , medivh);
+        	
+        MaitreDeLaPartie.jouerTour(jaina);
+        
+            
     }
 }
