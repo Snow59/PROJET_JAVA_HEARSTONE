3122333a914af542ddad8f6ce6886ce37731b490
Ibrahim <150235157+ibra2gang@users.noreply.github.com>
2024-01-31 00:18:33 +0100

    Ajout javadoc

--- HearStoneClasseChampion/CapaciteSpeciale.java
+++ HearStoneClasseChampion/CapaciteSpeciale.java
@@ -2,7 +2,9 @@ package HearStoneClasseChampion;
 
 import IMT_HearStone.Champion;
 
-
+/**
+ * Interface qui permet de definir l'attaque special
+ */
 public interface CapaciteSpeciale {
     void special(Champion cible);
 }
